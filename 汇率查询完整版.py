59fb48536a83d1b9401dd2918a208c843eb35ba3
Shanzheng Liu <liushanzheng960522@163.com>
2020-03-11 15:53:58 +1100

    Update 汇率查询完整版.py

--- 汇率查询完整版.py
+++ 汇率查询完整版.py
@@ -37,23 +37,7 @@ for k in range(2,29,1):
     x =  getinfor(x,k,html);
 
 
-# _user = "13430365600@163.com"
-# _pwd = "sam960522"
-# _to = ["379521061@qq.com","summerliu@live.com"];
-# x.encoding="utf8"
-# con = x.__str__()+"\n";
-# con = con + "政政实时汇率监控脚本使用中";
-# # 使用MIMEText构造符合smtp协议的header及body
-# msg = MIMEText(con,_charset="utf-8")
-# msg["Subject"] = u"当前exchange rate"
-# msg["From"] = _user
-# msg["To"] = ",".join(_to)
-#
-# s = smtplib.SMTP("smtp.163.com", timeout=30)  # 连接smtp邮件服务器,端口默认是25
-# s.login(_user, _pwd)  # 登陆服务器
-# s.sendmail(_user, _to, msg.as_string())  # 发送邮件
-# print "send succeed"
-# s.close()
+
 print x
 
 
